7608d7d38174567c9a9ba128b88c5a851929aa68
Lobanov_DD_RL6-69 <denlobanov5@mail.ru>
2024-05-23 17:44:29 +0300

    This is joke...I don't why I2C doesn't work, when it must work. I will buy PCB and wouldn't even be suurprised if it just proteus features

--- Comitas_hackathon_CMSIS/Debug/Src/ds18b20.d
+++ Comitas_hackathon_CMSIS/Debug/Src/ds18b20.d
@@ -1,19 +1,19 @@
 Src/ds18b20.o: ../Src/ds18b20.c \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/ds18b20.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/common.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f1xx.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f103x6.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/core_cm3.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_version.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_compiler.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_gcc.h \
- C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/system_stm32f1xx.h
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/ds18b20.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/common.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f1xx.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f103x6.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/core_cm3.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_version.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_compiler.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_gcc.h:
-C:/Users/denlo/Documents/stm32_projects/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/system_stm32f1xx.h:
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/ds18b20.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/common.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f1xx.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f103x6.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/core_cm3.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_version.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_compiler.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_gcc.h \
+ C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/system_stm32f1xx.h
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/ds18b20.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/Inc/common.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f1xx.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/stm32f103x6.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/core_cm3.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_version.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_compiler.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/cmsis_gcc.h:
+C:/Users/denlo/Documents/stm32_workspace/Comitas_hackathon/Comitas_hackathon_CMSIS/CMSIS/Inc/system_stm32f1xx.h:
